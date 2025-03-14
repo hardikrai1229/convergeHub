89e40a4eeded0561b392105825362193a05ec008
Uday Surothiya <udaysurothiya@gmail.com>
2025-03-15 04:04:09 +0530

    fixed-router-version

--- convergehub/frontend/src/index.js
+++ convergehub/frontend/src/index.js
@@ -1,11 +1,11 @@
 // src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client' instead of 'react-dom'
 import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root')); // Create a root using createRoot
+root.render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
